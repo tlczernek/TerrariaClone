c116472dae4fca7290d00b740019eb3b36fffc15
kumitsuuu _ <tlczernek@gmail.com>
2026-04-06 20:45:39 +0200

    ImGui setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
 
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(1920, 1080, "Window test");
 
+	rlImGuiSetup(true);
+
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.FontGlobalScale = 2;
+
 	while (!WindowShouldClose())
 		{
 		BeginDrawing();
 		ClearBackground(BLACK);
-		
+
+
+		//docking
+		rlImGuiBegin();
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+		ImGui::PopStyleColor(2);
+
+
 		
 		DrawRectangle(650, 650, 100, 100, PINK);
 		DrawRectangle(700, 700, 100, 100, { 255, 0, 0, 150 });
@@ -18,9 +35,32 @@ int main()
 
 		DrawText("Holy shit it's open", 600, 200, 40, RED);
 
+		ImGui::Begin("test");
+		
+		if (ImGui::Button("button")) {
+			std::cout << "Button pressed\n";
+		}
+
+		ImGui::End();
+
+
+		ImGui::Begin("second window");
+		ImGui::Text("window2");
+		ImGui::NewLine();
+		static float a = 0;
+		ImGui::SliderFloat("slider", &a, 0, 1);
+		ImGui::End();
+
+
+
+
+		rlImGuiEnd();
+		
 		EndDrawing();
 		}
 
+	rlImGuiShutdown();
+	
 	CloseWindow();
 
 	return 0;
